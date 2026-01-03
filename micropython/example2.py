cc0555f40b195077efb2df3dd2246c9cccf1aaf7
Jack Whitham <jack.d.whitham@gmail.com>
2026-01-03 10:40:59 +0000

    magic setsockopt

--- micropython/example2.py
+++ micropython/example2.py
@@ -1,19 +1,22 @@
 
-import asyncio
+import socket
+import time
 import wifi_settings
 
-async def serve_callback(reader: asyncio.StreamReader, writer: asyncio.StreamWriter) -> None:
-    try:
-        b = await reader.readexactly(1)
-        while b != b"\x04":
-            writer.write(b)
-            b = await reader.readexactly(1)
-        await writer.drain()
-        writer.close()
-    except EOFError:
-        pass
+MAGIC = 20
 
-async def amain() -> None:
+def recv_callback(s: socket.socket) -> None:
+    b = s.recv(1)
+    print("recv_callback", b)
+    if len(b) != 0:
+        s.send(b)
+
+def listen_callback(s: socket.socket) -> None:
+    (s2, v) = s.accept()
+    print("listen_callback", v)
+    s2.setsockopt(0, MAGIC, recv_callback)
+
+def main() -> None:
     print("Init")
     wifi_settings.init()
     print("Connect")
@@ -22,16 +25,16 @@ async def amain() -> None:
     ip = wifi_settings.get_ip()
     while not ip:
         print("Await IP address")
-        await asyncio.sleep(1)
+        time.sleep(1)
         ip = wifi_settings.get_ip()
 
     port = 2000
     print("Server", ip, port)
-    server = await asyncio.start_server(serve_callback, ip, port)
-    await asyncio.sleep(1000)
-
-def main() -> None:
-    asyncio.run(amain())
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((ip, port))
+    s.setsockopt(0, MAGIC, listen_callback)
+    s.listen(1)
+    print("OK")
 
 if __name__ == "__main__":
     main()
