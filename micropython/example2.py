37fed37b492932468dfb429fa28b744823357e03
Jack Whitham <jack.d.whitham@gmail.com>
2026-01-10 10:38:55 +0000

    notes

--- micropython/example2.py
+++ micropython/example2.py
@@ -1,61 +1,45 @@
+# asyncio TCP server example
+# 
+# Currently (v1.28) there is no support for UDP endpoints in Micropython asyncio.
+# Nor are there Protocols or Transports or Loop.create_server, so a TCP server
+# is started with asyncio.start_server.
+#
 
 import socket
-try:
-    # Running on MicroPython?
-    import wifi_settings
-    skip_wifi_settings = False
-except ImportError:
-    # Not running on MicroPython - use plain sockets library
-    skip_wifi_settings = True
-try:
-    import typing
-except ImportError:
-    pass
+import time
+import wifi_settings
+
 import asyncio
 
-class ProtocolForTCP(asyncio.Protocol):
-    transport: typing.Optional[asyncio.Transport] = None
-
-    def connection_made(self, transport: asyncio.BaseTransport) -> None:
-        if isinstance(transport, asyncio.Transport):
-            self.transport = transport
-
-    def data_received(self, data: bytes) -> None:
-        if self.transport:
-            self.transport.write(data)
-
-class ProtocolForUDP(asyncio.DatagramProtocol):
-    transport: typing.Optional[asyncio.DatagramTransport] = None
-
-    def connection_made(self, transport) -> None:
-        # note https://github.com/python/cpython/issues/90352 prevents proper type checking in Python <= 3.11
-        self.transport = transport
-
-    def datagram_received(self, data: bytes, addr: typing.Any) -> None:
-        if self.transport:
-            self.transport.sendto(data, addr)
+async def serve_callback(reader: asyncio.StreamReader, writer: asyncio.StreamWriter) -> None:
+    try:
+        b = await reader.readexactly(1)
+        while b != b"\x04":
+            writer.write(b)
+            b = await reader.readexactly(1)
+        await writer.drain()
+        writer.close()
+    except EOFError:
+        pass
 
 
 async def amain() -> None:
 
-    if skip_wifi_settings:
-        ip = "127.0.0.1"
-    else:
-        print("Init")
-        wifi_settings.init()
-        print("Connect")
-        wifi_settings.connect()
+    print("Init")
+    wifi_settings.init()
+    print("Connect")
+    wifi_settings.connect()
+    port = 2000
+    ip = wifi_settings.get_ip()
+    while not ip:
+        print("Await IP address")
+        await asyncio.sleep(1)
         ip = wifi_settings.get_ip()
-        while not ip:
-            print("Await IP address")
-            await asyncio.sleep(1)
-            ip = wifi_settings.get_ip()
-
-    loop = asyncio.get_running_loop()
-    tcp_service = await loop.create_server(ProtocolForTCP, host=ip, port=1234)
-    await loop.create_datagram_endpoint(ProtocolForUDP, local_addr=(ip, 1234))
-    print("Servers:", ip)
-    await tcp_service.serve_forever()
+    print("Server", ip, port)
+    foo = await asyncio.start_server(serve_callback, ip, port)
+    print(foo)
+    print(dir(foo))
+    await asyncio.sleep(1000)
 
 def main() -> None:
     asyncio.run(amain())
