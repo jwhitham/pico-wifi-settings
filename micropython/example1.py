b64c815712117d01f5bd8069d7730841a6012681
Jack Whitham <jack.d.whitham@gmail.com>
2026-01-17 10:42:59 +0000

    more test programs

--- micropython/example1.py
+++ micropython/example1.py
@@ -8,13 +8,24 @@ wifi_settings.init()
 print("Connect")
 wifi_settings.connect()
 
-def test_handler(msg_type, data_buffer, input_data_size, input_parameter, arg):
+ID_TEST_HANDLER_1 = wifi_settings.ID_FIRST_USER_HANDLER + 1
+ID_TEST_HANDLER_2 = wifi_settings.ID_FIRST_USER_HANDLER + 2
+ID_TEST_HANDLER_3 = wifi_settings.ID_FIRST_USER_HANDLER + 3
+INT_MIN = -0x80000000
+INT_MAX = 0x7fffffff
 
-    if -input_parameter != input_data_size:
-        return (0, 1)
+def test_handler_1(msg_type, data_buffer, input_data_size, input_parameter, arg):
+    """This test handler echoes the input back, XOR'ing it with a constant 0xac.
+    The input parameter is expected to be -input_data_size.
+    The output value is -10 minus the number of 0x41 bytes in the input data."""
 
+    print("test_handler_1:", input_data_size, input_parameter)
+    assert msg_type == ID_TEST_HANDLER_1
     assert arg == 1234
-    print("Test size", input_data_size)
+    assert len(data_buffer) == wifi_settings.MAX_DATA_SIZE
+    if -input_parameter != input_data_size:
+        return (0, -1)
+
     value = -10
     for i in range(input_data_size):
         if data_buffer[i] == 0x41:
@@ -23,8 +34,49 @@ def test_handler(msg_type, data_buffer, input_data_size, input_parameter, arg):
 
     return (input_data_size, value)
 
-ID_USER = wifi_settings.ID_FIRST_USER_HANDLER
-wifi_settings.set_handler(ID_USER, test_handler, 1234)
+def test_handler_2(msg_type, data_buffer, input_data_size, input_parameter, arg):
+    """This test handler generates output. The input parameter specifies the
+    number of bytes to generate."""
+
+    print("test_handler_2:", input_data_size, input_parameter)
+    assert msg_type == ID_TEST_HANDLER_2
+    assert arg == "A"
+    assert len(data_buffer) == wifi_settings.MAX_DATA_SIZE
+
+    if 0 < input_parameter <= wifi_settings.MAX_DATA_SIZE:
+        for i in range(input_parameter):
+            data_buffer[i] = (i + 1) & 0xff
+
+    return (input_parameter, input_parameter ^ 1)
+
+def test_handler_3(msg_type, data_buffer, input_data_size, input_parameter, arg):
+    """This test handler produces no output bytes. It checks the bounds of the inputs and outputs."""
+
+    print("test_handler_3:", input_data_size, input_parameter)
+    assert msg_type == ID_TEST_HANDLER_3
+    assert arg == test_handler_3
+    if input_parameter == INT_MIN:
+        return (0, -1)
+    if input_parameter == INT_MAX:
+        return (0, -2)
+    if input_parameter == input_data_size:
+        return (0, -3)
+    if input_parameter == -1:
+        return (0, INT_MIN)
+    if input_parameter == -2:
+        return (0, INT_MAX)
+    if input_parameter == -3:
+        return (0, INT_MAX + 1)
+    if input_parameter == -4:
+        return (0, INT_MIN - 1)
+    if input_parameter == -5:
+        return (0, 0x123456789abcde)
+
+    return (0, -4)
+
+wifi_settings.set_handler(ID_TEST_HANDLER_1, test_handler_1, 1234)
+wifi_settings.set_handler(ID_TEST_HANDLER_2, test_handler_2, "A")
+wifi_settings.set_handler(ID_TEST_HANDLER_3, test_handler_3, test_handler_3)
 
 try:
     ip1 = "X"
--- micropython/test_test_handler.py
+++ micropython/test_test_handler.py
@@ -2,7 +2,14 @@ import asyncio
 import os
 import remote_picotool
 
-ID_USER = remote_picotool.ID_FIRST_USER_HANDLER
+ID_TEST_HANDLER_1 = remote_picotool.ID_FIRST_USER_HANDLER + 1
+ID_TEST_HANDLER_2 = remote_picotool.ID_FIRST_USER_HANDLER + 2
+ID_TEST_HANDLER_3 = remote_picotool.ID_FIRST_USER_HANDLER + 3
+MAX_DATA_SIZE = 4096
+SIZES = [16, 1, 15, 17, 0, 500, 511, 513, MAX_DATA_SIZE, MAX_DATA_SIZE - 1]
+INT_MIN = -0x80000000
+INT_MAX = 0x7fffffff
+
 
 async def remote_handler_run_test_handler() -> None:
     try:
@@ -10,11 +17,12 @@ async def remote_handler_run_test_handler() -> None:
         reader, writer = await remote_picotool.get_pico_connection(config)
         client = remote_picotool.Client(config.update_secret_hash, reader, writer)
 
-        for size in [16, 1, 15, 17, 0, 500, 511, 513, 4096, 4095]:
-            print("Test size", size, end="", flush=True)
+        for size in SIZES:
+            assert size <= MAX_DATA_SIZE
+            parameter = -size
+            print("test_handler_1", size, parameter, end="", flush=True)
             request_data = os.urandom(size)
             expected_result_data = bytearray(size)
-            parameter = -len(request_data)
             expected_result = -10
             for i in range(size):
                 if request_data[i] == 0x41:
@@ -22,10 +30,67 @@ async def remote_handler_run_test_handler() -> None:
                 expected_result_data[i] = request_data[i] ^ 0xac
 
             print(", sending", end="", flush=True)
-            (result_data, result_value) = await client.run(ID_USER, request_data, parameter)
+            (result_data, result_value) = await client.run(ID_TEST_HANDLER_1, request_data, parameter)
             print(", checking", len(result_data), result_value, end="")
             assert result_value == expected_result
             assert len(result_data) == size
+            assert result_data == bytes(expected_result_data)
+            print(", OK", flush=True)
+
+        for parameter in SIZES + [-1, MAX_DATA_SIZE + 1, INT_MIN, INT_MAX]:
+            size = 0
+            print("test_handler_2", size, parameter, end="", flush=True)
+            expected_result = parameter ^ 1
+            request_data = b""
+            if parameter < 0:
+                expected_result_data = bytearray(0)
+            elif parameter < MAX_DATA_SIZE:
+                expected_result_data = bytearray(parameter)
+            else:
+                expected_result_data = bytearray(MAX_DATA_SIZE)
+            for i in range(len(expected_result_data)):
+                expected_result_data[i] = (i + 1) & 0xff
+
+            print(", sending", end="", flush=True)
+            (result_data, result_value) = await client.run(ID_TEST_HANDLER_2, request_data, parameter)
+            print(", checking", len(result_data), result_value, end="")
+            assert result_value == expected_result
+            assert len(result_data) == len(expected_result_data)
+            assert result_data == bytes(expected_result_data)
+            print(", OK", flush=True)
+
+        for (parameter, expected_result) in {
+                INT_MIN: -1,
+                INT_MAX: -2,
+                0: -3,
+                -1: INT_MIN,
+                -2: INT_MAX,
+                -3: INT_MIN, # INT_MAX + 1
+                -4: INT_MAX, # INT_MIN - 1
+                -5: 0x789abcde, # truncated
+                1: -4,
+        }.items():
+            size = 0
+            print("test_handler_3", size, parameter, end="", flush=True)
+            request_data = b""
+            print(", sending", end="", flush=True)
+            (result_data, result_value) = await client.run(ID_TEST_HANDLER_3, request_data, parameter)
+            print(", checking", len(result_data), result_value, end="")
+            assert result_value == expected_result
+            assert len(result_data) == 0
+            print(", OK", flush=True)
+
+        for parameter in SIZES:
+            size = 0
+            print("test_handler_3", size, parameter, end="", flush=True)
+            request_data = b""
+            expected_result_data = bytearray(size)
+            expected_result = parameter
+            print(", sending", end="", flush=True)
+            (result_data, result_value) = await client.run(ID_TEST_HANDLER_3, request_data, parameter)
+            print(", checking", len(result_data), result_value, end="")
+            assert result_value == expected_result, (result_value, expected_result)
+            assert len(result_data) == len(expected_result_data)
             assert result_data == bytes(expected_result_data)
             print(", OK", flush=True)
 
